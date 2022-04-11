8cdf102c226a64fa587119899d2d56972633ae54
Alexandra Szűcs <alexandra.szucs93@gmail.com>
2022-04-11 15:05:47 +0200

    c5

--- src/Exercise1C.java
+++ src/Exercise1C.java
@@ -1,13 +1,10 @@
 public class Exercise1C {
 
     public static void main(String[] args) {
-        cheer();
+        cheer("Alexandra", 28);
     }
 
-
-    public static void cheer() {
-        System.out.println("Hungarian");
-
-
+    public static void cheer(String name, Integer age) {
+        System.out.println("My name is " + name + ", I am " + age + " years old.");
     }
 }
