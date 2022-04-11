cffa8871901706472b413131a8da05413e363c04
nagymihalyi <rubin.schwarz@freemail.hu>
2022-04-11 15:08:17 +0200

    a5

--- src/Greeting.java
+++ src/Greeting.java
@@ -1,6 +1,7 @@
 public class Greeting {
 
-  public static void greet() {
-    System.out.println("Greetings!");
+  public static void greet(String text1, String text2) {
+    System.out.println(text1);
+    System.out.println(text2);
   }
 }
